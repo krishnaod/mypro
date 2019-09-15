80bc4c65cdea877f0a634ae4368cf00842c5de9e
Krishna Prajapati <prajapatikc@gmail.com>
2019-09-15 11:15:00 +0000

    script modified

--- dock/script/install_docker_ubuntu18.sh
+++ dock/script/install_docker_ubuntu18.sh
@@ -17,4 +17,4 @@ docker --version
 # Docker Compose
 sudo curl -L https://github.com/docker/compose/releases/download/1.24.1/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
-docker-compose –version
+docker-compose --version
